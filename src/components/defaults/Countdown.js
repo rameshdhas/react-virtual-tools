090b630d6cf3fa060311982b485da2b6480fe67b
rameshdhason <drameshdhas@gmail.com>
2023-03-11 15:24:01 -0500

    Initial commit

--- src/components/defaults/Countdown.js
+++ src/components/defaults/Countdown.js
@@ -3,6 +3,10 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
 const Root = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   font-family: Menlo, monospace;
   font-size: 100px;
   text-shadow: 1px 2px rgba(0, 0, 0, 0.5);
